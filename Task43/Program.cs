85ca8d9d4e8c65188baef0967f05112a7540bf82
Николай <nnm_58@mail.ru>
2023-10-08 14:03:56 +0300

    Задача 43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,52 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-// Задача 44: Не используя рекурсию, выведите первые N чисел 
-// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
-// Если N = 5 -> 0 1 1 2 3
-// Если N = 3 -> 0 1 1
-// Если N = 7 -> 0 1 1 2 3 5 8
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
 
-int Prompt(string message)
-{
-    System.Console.Write(message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
-}
-
-int[] FiboArray(int num)
-{
-    int[] arr = new int[num];
-    //arr[0] = 0; 
-    arr[1] = 1;
-    for (int i = 2; i < arr.Length; i++)
-    {
-        arr[i] = arr[i - 1] + arr[i - 2];
-    }
-    return arr;
-}
-
-void PrintArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-        else Console.Write($"{arr[i]} ");
-    }
-
-}
-
-int number = Prompt("Введите число больше 1 : ");
-if (number < 2)
-{
-    Console.WriteLine("Некорректный ввод.");
-    return;
-}
-int[] fiboArray = FiboArray(number);
-
-PrintArray(fiboArray);
-
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
