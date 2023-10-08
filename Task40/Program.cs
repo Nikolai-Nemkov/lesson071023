7ac39626db29468118e759d4ca67a615252d61eb
Николай <nnm_58@mail.ru>
2023-10-08 13:18:43 +0300

    Задача 42

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -28,31 +28,4 @@ Console.WriteLine(res ? "Треугольник существует": "Треу
 
 
 
-Задача 42: Напишите программу, которая будет преобразовывать
-десятичное число в двоичное.
-46 -> 101110
-13 -> 1101
-2 -> 10
-
-// Задача 42: Напишите программу, которая будет преобразовывать
-// десятичное число в двоичное.
-// 46 -> 101110
-// 13 -> 1101
-// 2 -> 10
-
-Console.Write("Can you enter number:");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(DecimalToBinary(number));
-
-int DecimalToBinary(int num)
-{
-    int result = 0;
-    int mult = 1;
-    while (num > 0)
-    {
-        result += num % 2 * mult;
-        num = num / 2;// num /= 2 //
-        mult *= 10;
-    }
-    return result;
-}
+ 
