0d474129400f4c6ed2318ef1b607ad9371d7dc9f
Николай <nnm_58@mail.ru>
2023-10-08 13:10:08 +0300

    Папка 39

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Задача 39: Напишите программу, которая перевернёт
+/*Задача 39: Напишите программу, которая перевернёт
 одномерный массив (последний элемент будет на первом
 месте, а первый - на последнем и т.д.)
 [1 2 3 4 5] -> [5 4 3 2 1]
-[6 7 3 6] -> [6 3 7 6]
+[6 7 3 6] -> [6 3 7 6] */
 
 int[] CreateArrayRndInt(int size, int min, int max)
 {
@@ -43,8 +43,6 @@ void ReverseArray(int[] arr)
         arr[arr.Length - 1 - i] = temp;
     }
 }
-
-
 
 int[] array = CreateArrayRndInt(5, 1, 9);
 PrintArray(array);
