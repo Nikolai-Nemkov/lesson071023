9496f3140765e33c58f60daaa720711c5f3a69fa
Николай <nnm_58@mail.ru>
2023-10-09 19:38:35 +0300

    Задача 42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача 42");
 
 // Задача 42: Напишите программу, которая будет преобразовывать
 // десятичное число в двоичное.
@@ -11,20 +11,19 @@ Console.Write("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(DecimalToBinary(number));
 
-int DecimalToBinary(int num)   // DecimalToBinary перевод как "ДесятичноеДвоичное"
+int DecimalToBinary(int num) // DecimalToBinary перевод как "ДесятичноеДвоичное"
 {
     int result = 0;
     int mult = 1;
-    while (num > 0)    // метод for используем когда нам известно количество операций
-                               // а while когда неизвестно.
+    while (num > 0) // метод for используем когда нам известно количество операций
+    // а while когда неизвестно.
     {
-        result += num % 2 * mult;   // цикл деления будет до 0. если при делении будет четное, ставим -0, нечетное -1
-        num = num / 2;              // num /= 2 
-        mult *= 10;        // 
+        result += num % 2 * mult; // цикл деления будет до 0. если при делении будет четное, ставим -0, нечетное -1
+        num = num / 2; // num /= 2
+        mult *= 10; //
     }
     return result;
 }
-
 
 /*
 2й вариант решения, меняем метод.
@@ -42,4 +41,3 @@ string DecimalToBinaryStr(int num)
 }
 
 */
-
